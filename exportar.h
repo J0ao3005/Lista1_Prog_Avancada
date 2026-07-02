657dcfc5846b9b3661358e097f70322dc5ad9d25
João Henrique Pedrosa de Souza <joao.hps@aluno.ufop.edu.br>
2026-07-02 20:00:41 -0300

    Delete exportar.h

--- exportar.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXPORTAR_H
-#define EXPORTAR_H
-
-#include "arvore.h"
-
-// Exporta todos os segmentos da árvore no formato CSV (x1,y1,x2,y2)
-void exporta_csv(ptrNo raiz, const char *filename);
-
-#endif
